296c7f8ab583a8ab75f3f0d07a88cd07acfbde92
AkshajJain <akshajj2004@gmail.com>
2021-07-15 15:34:45 +0530

    chore: add consts.py containing some boilerplate code
    consts.py holds constants and common functions which may be required by various other files
    
    Signed-off-by: AkshajJain <akshajj2004@gmail.com>

---
+++ consts.py
@@ -0,0 +1,19 @@
+import bz2
+
+DBG = 0
+PROJECTS_FOLDER = "User_Projects"
+ALLOWED_CHARS = [chr(ord('a') + i) for i in range(26)] + [chr(ord('A') + i) for i in range(26)] + [str(i) for i in range(10)] + ["_"]
+
+def dbg( msg_priority , *args , **kwargs ):
+    if msg_priority < DBG:
+        print( *args, **kwargs )
+
+def compress( _data ):
+    bz2.compress(_data)
+
+def validate_name(_name):
+    for _char in _name:
+        if _char not in ALLOWED_CHARS:
+            print("Input name:", _name, "is not valid")
+            return False
+    return True
