b90445672fcab76747a6a4f1357f4ba9c48b3fb5
AkshajJain <akshajj2004@gmail.com>
2021-07-15 18:52:47 +0530

    style: modify db_manager.py to follow the code style
    Signed-off-by: AkshajJain <akshajj2004@gmail.com>

--- db_manager.py
+++ db_manager.py
@@ -16,15 +16,15 @@ class DBManager:
 
     def connect( self, _project_name ):
         projects    = os.listdir( consts.PROJECTS_FOLDER )
-        db_exists   = (_project_name + ".db") in projects
+        db_exists   = ( _project_name + ".db" ) in projects
         self.db     = consts.PROJECTS_FOLDER + "/" + _project_name + ".db"
         self.conn   = sqlite3.connect( self.db )
         self.cursor = self.conn.cursor( )
         if not db_exists:
-            self.cursor.execute( "CREATE TABLE IF NOT EXISTS 'Cards'('uid' UNSIGNED BIGINT PRIMARY KEY, 'data' TEXT);" )
-            self.cursor.execute( "CREATE TABLE IF NOT EXISTS 'Boards'('uid' UNSIGNED BIGINT PRIMARY KEY, 'info' TEXT, 'data' TEXT);" )
-            self.cursor.execute( "CREATE TABLE IF NOT EXISTS 'Info'('project_data' TEXT, 'board_last_used_uid' UNSIGNED BIGINT, 'card_last_used_uid' UNSIGNED BIGINT);" )
-            self.cursor.execute( "INSERT INTO `Info` (`board_last_used_uid`, `card_last_used_uid`) VALUES ('0', '0');" )
+            self.cursor.execute( "CREATE TABLE IF NOT EXISTS 'Cards'( 'uid' UNSIGNED BIGINT PRIMARY KEY, 'data' TEXT );" )
+            self.cursor.execute( "CREATE TABLE IF NOT EXISTS 'Boards'( 'uid' UNSIGNED BIGINT PRIMARY KEY, 'info' TEXT, 'data' TEXT );" )
+            self.cursor.execute( "CREATE TABLE IF NOT EXISTS 'Info'( 'project_data' TEXT, 'board_last_used_uid' UNSIGNED BIGINT, 'card_last_used_uid' UNSIGNED BIGINT );" )
+            self.cursor.execute( "INSERT INTO `Info` ( `board_last_used_uid`, `card_last_used_uid` ) VALUES ( '0', '0' );" )
             self.conn.commit( )
 
     def get_next_board_uid( self ):
@@ -37,45 +37,46 @@ class DBManager:
     def get_next_card_uid( self ):
         self.cursor.execute( "SELECT `card_last_used_uid` FROM `Info`;" )
         fetched_data  = self.cursor.fetchall( )
+        l =
         next_card_uid = fetched_data[ 0 ][ 0 ] + 1
         consts.dbg( 1, "Class DBManager - function get_next_card_uid - value of next_card_uid:", next_card_uid )
         return next_card_uid
 
     def add_board( self, _board_uid, _board_info ):
         consts.dbg( 1, "Class DBManager - function add_board - value of _board_uid:", _board_uid )
-        self.cursor.execute( "INSERT INTO `Boards` (`uid`, `info`) VALUES (?, ?);", (_board_uid, _board_info,) )
-        self.cursor.execute( "UPDATE `Info` SET `board_last_used_uid` = ?;", (_board_uid,) )
+        self.cursor.execute( "INSERT INTO `Boards` ( `uid`, `info` ) VALUES ( ?, ? );", ( _board_uid, _board_info, ) )
+        self.cursor.execute( "UPDATE `Info` SET `board_last_used_uid` = ?;", ( _board_uid, ) )
         self.conn.commit( )
 
     def add_card( self, _card_uid, _card_data ):
         consts.dbg( 1, "Class DBManager - function add_card - value of _card_uid:", _card_uid )
-        self.cursor.execute( "INSERT INTO `Cards` (`uid`, `data`) VALUES (?, ?);", (_card_uid, _card_data) )
-        self.cursor.execute( "UPDATE `Info` SET `card_last_used_uid` = ?;", (_card_uid,) )
+        self.cursor.execute( "INSERT INTO `Cards` ( `uid`, `data` ) VALUES ( ?, ? );", ( _card_uid, _card_data ) )
+        self.cursor.execute( "UPDATE `Info` SET `card_last_used_uid` = ?;", ( _card_uid, ) )
         self.conn.commit( )
 
     def update_project_info( self, _project_data ):
-        self.cursor.execute( "UPDATE `Info` SET `project_data` = ?;", (_project_data,) )
+        self.cursor.execute( "UPDATE `Info` SET `project_data` = ?;", ( _project_data, ) )
         self.conn.commit( )
 
     def update_board_info( self, _board_uid, _board_data ):
-        self.cursor.execute( "UPDATE `Boards` SET `info` = ? WHERE `uid` = ?;", (_board_data, _board_uid) )
+        self.cursor.execute( "UPDATE `Boards` SET `info` = ? WHERE `uid` = ?;", ( _board_data, _board_uid ) )
         self.conn.commit( )
 
     def update_board_data( self, _board_name, _column_name, _column_cards ):
-        self.cursor.execute( "UPDATE `Boards` SET `data` = ? WHERE `uid` = ?;", (_column_cards, _column_name) )
+        self.cursor.execute( "UPDATE `Boards` SET `data` = ? WHERE `uid` = ?;", ( _column_cards, _column_name ) )
         self.conn.commit( )
 
     def update_card( self, _card_uid, _card_data ):
-        self.cursor.execute( "UPDATE `Cards` SET `data` = ? WHERE `uid` = ?;", (_card_data, _card_uid) )
+        self.cursor.execute( "UPDATE `Cards` SET `data` = ? WHERE `uid` = ?;", ( _card_data, _card_uid ) )
         self.conn.commit( )
 
     def delete_board( self, _board_uid ):
-        self.cursor.execute( "DELETE FROM `Boards` WHERE `uid` = ?;", (_board_uid,) )
+        self.cursor.execute( "DELETE FROM `Boards` WHERE `uid` = ?;", ( _board_uid, ) )
         self.conn.commit( )
 
     def delete_card( self, _card_uid ):
         # Delete card completely
-        self.cursor.execute( "DELETE FROM `Cards` WHERE `uid` = ?;", (_card_uid,) )
+        self.cursor.execute( "DELETE FROM `Cards` WHERE `uid` = ?;", ( _card_uid, ) )
         self.conn.commit( )
 
     def get_project_info( self ):
@@ -86,7 +87,7 @@ class DBManager:
         return project_info
 
     def get_board_info( self, _board_uid ):
-        self.cursor.execute( "SELECT `info` FROM `Boards` WHERE `uid` = ?;", (_board_uid,) )
+        self.cursor.execute( "SELECT `info` FROM `Boards` WHERE `uid` = ?;", ( _board_uid, ) )
         fetched_data = self.cursor.fetchall( )
         board_info   = fetched_data[ 0 ][ 0 ]
         consts.dbg( 1, "Class DBManager - function get_board_info - value of board_info:", board_info )
@@ -99,7 +100,6 @@ class DBManager:
         for i in fetched_data:
             for board in i:
                 all_boards_info.append( board )
-
         consts.dbg( 1, "Class DBManager - function get_all_boards_info - value of all_boards_info:", all_boards_info )
         return all_boards_info
 
@@ -110,7 +110,6 @@ class DBManager:
         for i in fetched_data:
             for card in i:
                 board_data.append( card )
-
         consts.dbg( 1, "Class DBManager - function get_board_data - value of board_data:", board_data )
         return board_data
 
@@ -121,12 +120,11 @@ class DBManager:
         for i in fetched_data:
             for card in i:
                 all_boards_uid.append( card )
-
         consts.dbg( 1, "Class DBManager - function get_all_boards_uid - value of all_boards_uid:", all_boards_uid )
         return all_boards_uid
 
     def get_card_from_uid( self, _card_uid ):
-        self.cursor.execute( "SELECT `data` FROM `Cards` WHERE `uid` = ?;", (_card_uid,) )
+        self.cursor.execute( "SELECT `data` FROM `Cards` WHERE `uid` = ?;", ( _card_uid, ) )
         fetched_data = self.cursor.fetchall( )
         card         = fetched_data[0][0]
         consts.dbg( 1, "Class DBManager - function get_card_from_uid - value of card:", card )
@@ -139,7 +137,6 @@ class DBManager:
         for i in fetched_data:
             for card in i:
                 all_cards.append( card )
-
         consts.dbg( 1, "Class DBManager - function get_all_cards - value of all_cards:", all_cards )
         return all_cards
 
@@ -150,7 +147,6 @@ class DBManager:
         for i in fetched_data:
             for card in i:
                 all_cards_uid.append( card )
-
         consts.dbg( 1, "Class DBManager - function get_all_cards_uid - value of all_cards_uid:", all_cards_uid )
         return all_cards_uid
 
